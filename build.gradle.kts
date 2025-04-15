80ecb1cba0c3bfb36b9742ab7d3a860bdd1d8f56
bolt512 <go.lang@icloud.com>
2025-04-15 22:45:48 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "com.bolt512.kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
